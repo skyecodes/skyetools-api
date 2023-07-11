3bfe59f75d120aa8aa4012787812fe032ce59ed6
skyecodes <skyecodes@proton.me>
2023-07-11 15:12:06 +0200

    feat: allow multiple origin patterns

--- src/main/kotlin/com/skyecodes/skyetools/WebConfig.kt
+++ src/main/kotlin/com/skyecodes/skyetools/WebConfig.kt
@@ -20,7 +20,7 @@ class WebConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns(frontUrl)
+            .allowedOriginPatterns(*frontUrl.split(" ", ";").toTypedArray())
             .allowedMethods("*")
     }
 
