5481a43a556f3d4c8be196a8ee09e8189755b4d9
skyecodes <skyecodes@proton.me>
2023-09-11 16:50:16 +0200

    fix: converter not working with images

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.1.1"
-    id("io.spring.dependency-management") version "1.1.0"
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
     kotlin("jvm") version "1.9.0"
     kotlin("plugin.spring") version "1.9.0"
 }
 
 group = "com.skyecodes"
-version = "0.3.1"
+version = "0.3.2"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
